c828b789d354b21d165d40f4c6fc6550c19c043e
Bruno Rocha <brunoxthe2@hotmail.com>
2020-01-06 16:58:40 -0300

    raphael: bootleg initial bringup

--- bootleg_raphael.mk
+++ bootleg_raphael.mk
@@ -6,20 +6,30 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Bootleggers stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_raphael
+PRODUCT_NAME := bootleg_raphael
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
+
+#Maintainer
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS=rochadev
+
+#Make it official
+BOOTLEGGERS_BUILD_TYPE=Shishufied
+
+#select Bootanimation
+TARGET_PICK_BOOTANIMATION=2,5,9
+
+#use Lawnchair Launcher
+BOOTLEGGERS_SITDOWN :=true
