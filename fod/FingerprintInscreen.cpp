3b1d9be2aa39e80f1e22bae53a3876dcf30bf335
Bruno Rocha <brunoxthe2@hotmail.com>
2020-01-06 17:25:46 -0300

    raphael: fod: set calculated icon local

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -31,9 +31,9 @@
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-#define FOD_SENSOR_X 445
-#define FOD_SENSOR_Y 1931
-#define FOD_SENSOR_SIZE 190
+#define FOD_SENSOR_X 453
+#define FOD_SENSOR_Y 1918
+#define FOD_SENSOR_SIZE 174
 
 #define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness_clone"
 
